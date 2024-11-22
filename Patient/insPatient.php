86056ec05814eadd9940a817b5228a61dc1558c8
BabacarFaye <bf322003@gmail.com>
2024-11-22 18:31:56 +0100

    nouveu dossier configuration et le fichier index.php

--- Patient/insPatient.php
+++ Patient/insPatient.php
@@ -28,7 +28,7 @@ if(isset($_POST["ok"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page d'accueil</title>
+    <title>carnet-de-santé</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.6.0/css/fontawesome.min.css">
     <style>
@@ -48,7 +48,7 @@ if(isset($_POST["ok"])){
   <header>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">CSN</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -58,13 +58,13 @@ if(isset($_POST["ok"])){
           <a class="nav-link active" aria-current="page" href="index.html">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Profil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">ins</a>
+          <a class="nav-link" href="#">Mes Rendez-vous</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="#">Mes documents</a>
         </li>
       </ul>
     </div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include 'configuration/head.php';
+?>
+<h1>Bienvenue dans votre carnet de santé numerique</h1>
+
+
+
+
+
+
+<?php
+include 'configuration/pied.php';
+?>
