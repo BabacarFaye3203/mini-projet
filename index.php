8385d5d841939ed9fced151c1a2f3a0c9aeab8fe
Tigo950 <felixngamakita29@gmail.com>
2024-11-27 20:02:29 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'acceuil du Carnet de Santé Numérique</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="logo"> <span>C</span>SN</div>
+        <ul class="menu">
+            <a href="#">ACCEUIL</a>
+            <a href="#">Service Médical</a>
+            <a href="#">A PROPOS</a>          
+            <a href="#">Témoignages</a>
+            <a href="#">Contact</a>
+        </ul>
+    </header>
+
+    <!-- Slide -->
+
+    <section class="slide">
+        <div class="slide-infos">
+            <h1>Votre Santé Notre Priorité</h1>
+            <p>Bénéficiez de nos services en vous inscrivons sur note site </p>
+            <a href="#">Sign up</a>
+        </div>
+    </section>
+
+    <!-- service Médical -->
+
+    <section class="service">
+        <h1 class="title">SERVICE MEDICAL</h1>
+        <p class="sub-title"><p><CENTER> Ce site vous offre à vous, et à votre famille, la gamme complète des services de santé en ligne.</CENTER></p>
+        <div class="liste-service">
+            <div class="box">
+                <h1>Cardiologie</h1>
+                <p>la cardiologie numérique permet une gestion proactive de la santé cardiovasculaire, offrant ainsi une meilleure prise en charge, un suivi plus précis et des interventions plus ciblées pour prévenir les maladies cardiaques.</p>
+            </div>
+            <div class="box">
+                <h1>DENTISTE</h1>
+                <p>Résolvez vos problèmes dentaire en prenant contact avec nos Spécialistes</p>
+            </div>
+            <div class="box">
+                <h1>GYNECOLOGIE</h1>
+                <p>Pour votre santé reproductive et sexuelle, Consultez les gynécologues</p>
+            </div>
+            <div class="box">
+                <h1>PEDIATRIE</h1>
+                <p>Pour le bien ètre de vos enfants le CSN met un service de Pédiatrie à votre disposition.</p>
+            </div>
+            <div class="box">
+                <h1>OPHTAMOLOGIE</h1>
+                <p>Pour vos tests de la vue, fonds d'œil, pression oculaire... Veuillez consultez nos Ophatmologues.</p>
+            </div>
+            <div class="box">
+                <h1>MEDECINE GENERALE</h1>
+                <p>Pour tout autres services veuillez nous contactez.</p>
+            </div>
+         
+        </div>
+    </section>
+    
+    <!-- A propos -->
+    <section class="a-propos"></section>
+
+    <!-- Notre But -->
+
+    <section class="notre-objectif">
+        <div class="left">
+            <span>le Carnet de Santé numérique</span>
+            <h1>Plateforme qui permet au patient de réaliser la prise de rendez-vous depuis son appareil électronique, afin d’effectuer une téléconsultation avec le médecin de son choix.</h1>
+            <a href="#">Prendre rendez-vous en vous inscrivant</a>
+        </div>
+        <div class="right">
+            <p>Le carnet de santé numérique est un outil innovant qui centralise et sécurise toutes les informations médicales d'un individu, permettant un suivi de santé plus personnalisé et accessible.</p>
+            <p> Il regroupe les données relatives aux antécédents médicaux, aux traitements en cours, aux résultats d'examens, aux vaccinations et aux rendez-vous médicaux. Ce carnet facilite la communication entre le patient et les professionnels de santé, et permet un accès rapide à l’historique médical, où que l'on soit.</p>
+        </div>
+    </section>
+
+    <!-- footer-contact -->
+
+    <footer>
+        <h1 class="title">Contactez-nous</h1>
+        <div class="contact">
+            <div class="class-form">
+                <h2>ENTREZ VOS PREOCCUPATIONS</h2>
+                <h4>VALABLE PENDANT 24H !</h4>
+                <form action="">
+                    <input type="text" placeholder="Entre votre nom">
+                    <input type="email" placeholder="Entrer votre mail">
+                    <textarea placeholder="Entrer votre message" cols="30" rows="8"></textarea>
+                    <button>SUBMIT</button>
+                </form>
+            </div>
+            <div class="addresse">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.874102365142!2d2.335455315619297!3d48.860611079287644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671d877937b0f%3A0xb975fcfa192f84d4!2sMus%C3%A9e%20du%20Louvre!5e0!3m2!1sfr!2sfr!4v1653640866666!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>  
+    </footer>
+    <div class="fin">
+        <p>Réalisé par @Babacar, @Emmanuel, @Félix</p>
+    </div>
+</body>
+</html>
+
