d1de8198186da1884c0e1fc771a279a191c87de8
loubah19 <eloubahasra@gmail.com>
2024-11-30 04:12:55 +0100

    MISES A JOURS

--- medecin/Gest_RDV.php
+++ medecin/Gest_RDV.php
@@ -33,75 +33,109 @@ $result2=$result;
 <?php //include '../configuration/headPatient.php';
 include '../configuration/head.php';
 ;?>
-
-<p class="d-inline-flex gap-1">
-    <a class="btn btn-primary" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Voici la liste de vos RDV </a>
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Rendez-vous Accepter</button>
-</p>
-<div class="row">
-    <div class="col">
-        <div class="collapse multi-collapse" id="multiCollapseExample1">
-            <div class="card card-body">
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
-                <?php echo "<div style='padding:10% 200px 0% 200px ;
-                margin:8% 23px 10% auto ;'>";?>
-                <?php if($result->num_rows > 0) { ?>
-                    <table class='table'>
-                        <thead class='table-dark'>
-                        <tr><th></th><th>Nom</th><th>Prénom</th><th>Details</th></tr>
-                        </thead>
-                        <?php ?>
-                        <?php $i=0; while ($row = $result->fetch_assoc()) { $i++;
-                            echo "<tr>
-            <td> $i</td>";?>
-                            <td><?= htmlspecialchars($row['nomPatient']); ?></td>
-                            <td><?= htmlspecialchars($row['pren']); ?></td>
-                            <td><form action="voirPatient.php" method="post" style="display: inline;">
-                                    <input type="hidden" name="idP" value="<?= $row['idP_Patient']; ?>">
-                                    <input type="submit" value="Details" class="btn btn-info" name="Detail">
-                                </form>
-                            </td>
-                            <?php echo "</tr>";?>
-                        <?php } ?>
-                    </table>
-                <?php }?>
-
-                <?php echo "</div>"?>
-            </div>
-        </div>
+<div class="d-flex align-items-start">
+    <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+        <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+        <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+        <button class="nav-link" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled" type="button" role="tab" aria-controls="v-pills-disabled" aria-selected="false" disabled>Disabled</button>
+        <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+        <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
     </div>
-    <div class="col">
-        <div class="collapse multi-collapse" id="multiCollapseExample2">
-            <div class="card card-body">
-                <?php echo "<div style='padding:10% 200px 0% 200px ;
-                margin:8% 23px 10% auto ;'>";?>
-                <?php if($result->num_rows > 0) { ?>
-                    <table class='table'>
-                        <thead class='table-dark'>
-                        <tr><th></th><th>Nom</th><th>Prénom</th><th>Details</th></tr>
-                        </thead>
-                        <?php ?>
-                        <?php $i=0; while ($row = $result->fetch_assoc()) { $i++;
-                            echo "<tr>
-            <td> $i</td>";?>
-                            <td><?= htmlspecialchars($row['nomPatient']); ?></td>
-                            <td><?= htmlspecialchars($row['pren']); ?></td>
-                            <td><form action="voirPatient.php" method="post" style="display: inline;">
-                                    <input type="hidden" name="idP" value="<?= $row['idP_Patient']; ?>">
-                                    <input type="submit" value="Details" class="btn btn-info" name="Detail">
-                                </form>
-                            </td>
-                            <?php echo "</tr>";?>
-                        <?php } ?>
-                    </table>
-                <?php }?>
-
-                <?php echo "</div>"?>
-                Some placeholder content for the second collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
-            </div>
-        </div>
+    <div class="tab-content" id="v-pills-tabContent">
+        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="v-pills-disabled" role="tabpanel" aria-labelledby="v-pills-disabled-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">...</div>
     </div>
 </div>
+<div style='padding:10% 200px 0% 650px ;margin:8% 23px 10% auto ;'>";
+<div class="container d-grid gap-2 my-4 ">
+    <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+        LISTE DES RDV
+    </a>
+</div>
+
+<div class="collapse" id="collapseExample">
+    <div class="card card-body">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <?php if ($result->num_rows > 0) { ?>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead class="table-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $i = 0; while ($row = $result->fetch_assoc()) { $i++; ?>
+                        <tr>
+                            <td><?= $i; ?></td>
+                            <td><?= htmlspecialchars($row['nomPatient']); ?></td>
+                            <td><?= htmlspecialchars($row['pren']); ?></td>
+                            <td>
+                                <form action="voirPatient.php" method="post" class="d-inline">
+                                    <input type="hidden" name="idP" value="<?= $row['idP_Patient']; ?>">
+                                    <button type="submit" class="btn btn-info btn-sm" name="Detail">Details</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php } else { ?>
+            <p class="text-center text-muted">Aucun rendez-vous trouvé.</p>
+        <?php } ?>
+    </div>
+</div>
+
+<div class="container d-grid gap-2 my-4">
+    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1">
+        Rendez-vous Acceptés
+    </button>
+</div>
+
+<div class="collapse" id="collapseExample1">
+    <div class="card card-body">
+        <?php if ($result->num_rows > 0) { ?>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead class="table-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $i = 0; while ($row = $result->fetch_assoc()) { $i++; ?>
+                        <tr>
+                            <td><?= $i; ?></td>
+                            <td><?= htmlspecialchars($row['nomPatient']); ?></td>
+                            <td><?= htmlspecialchars($row['pren']); ?></td>
+                            <td>
+                                <form action="voirPatient.php" method="post" class="d-inline">
+                                    <input type="hidden" name="idP" value="<?= $row['idP_Patient']; ?>">
+                                    <button type="submit" class="btn btn-info btn-sm" name="Detail">Details</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php } else { ?>
+            <p class="text-center text-muted">Aucun rendez-vous accepté trouvé.</p>
+        <?php } ?>
+    </div>
+</div>
+</div>
+
 <?php
 ?>
 <?php
